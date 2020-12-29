fbf4b83612a70c506976162532728ffdad4b9ab8
Wade Striebel <wade@striebel.ca>
2020-12-28 20:51:38 -0500

    Combine Discourse and Fider changes with a progress bar

---
+++ app/Commands/ImportFiderToDiscourse.php
@@ -0,0 +1,107 @@
+<?php
+
+
+namespace App\Commands;
+
+use App\Service\Discourse;
+use App\Service\Fider;
+use Illuminate\Support\Facades\File;
+use LaravelZero\Framework\Commands\Command;
+
+class ImportFiderToDiscourse extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'import';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Import posts from Fider to Discourse';
+
+
+    /**
+     * Throttle Variables
+     */
+    private $maxRequestsPerMinute = 20; // 20 is the default Discourse rate limit
+    private $requestCount = 0;
+    private $firstRequestTime = null;
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Get the file path from the user.
+        $path = $this->ask('What is the path to your Fider posts export file?');
+
+
+        // Check it exists and we can read it.
+        if(! File::exists($path) || ! File::isReadable($path)) {
+            $this->error('We cannot find the file or it is not readable');
+            return false;
+        }
+
+        // Get the file
+        $file = File::get($path);
+
+        // Convert the files json to a collection
+        $array = json_decode($file, true);
+        $posts = collect($array);
+
+        // Check it matches the structure we expect
+        if (! isset($posts->first()['title']) && ! isset($posts->first()['description']) ) {
+            $this->error('We cannot seem to parse your file, is it a post export from fider?');
+            return false;
+        }
+
+        // Warn about time expectation with Discourse Rate Limiting
+        $count = $posts->count();
+        $minutes = ceil($count/60);
+        $this->warn("With {$count} Fider posts to be imported it may take up to {$minutes} minutes to finish the import due to the API rate limit by Discourse.");
+
+        // Loop over the collection and create Discourse threads for each post
+        $this->withProgressBar($posts, function ($post) {
+            // Actually create the Discourse Post
+            $fiderPost = $this->getFiderPost($post);
+            $this->createDiscoursePost($fiderPost);
+        });
+
+        // Let the user know we finished
+        $this->newLine();
+        $this->info('Finished importing the posts from Fider to Discourse.');
+    }
+
+    private function getFiderPost($post)
+    {
+        $fider = new Fider();
+
+        return $fider->getPostData($post);
+    }
+
+    private function createDiscoursePost($post)
+    {
+        // Discourse has a rate limit of 20 API requests
+        // per minute so we will throttle the creates
+        if ($this->requestCount >= $this->maxRequestsPerMinute) {
+            sleep(60);
+            $this->firstRequestTime = null;
+            $this->requestCount = 0;
+        }
+
+        if ($this->firstRequestTime === null) {
+            $this->firstRequestTime = microtime(true);
+        }
+
+        $this->requestCount++;
+
+        app(Discourse::class)->test();
+    }
+}
